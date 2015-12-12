4e06d316b9dcf70defc2e72ea19e1305fee928d7
Omar Faruqe <faruqe.cse@gmail.com>
2015-12-11 21:19:36 -0500

    Initial Commit

---
+++ RestiOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestiOSApp
+//
+//  Created by Omar Faruqe on 2015-12-11.
+//  Copyright © 2015 Omar Faruqe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
